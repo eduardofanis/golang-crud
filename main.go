bf24db5130f3d52318bd163f1f0ed27cf2687f56
Eduardo Fanis <eduardo.fanis@hotmail.com>
2024-03-28 19:18:00 -0300

    feat: init routes

--- main.go
+++ main.go
@@ -3,22 +3,23 @@ package main
 import (
 	"log"
 
-	"github.com/fvnis/golang-crud/src/controller/routes"
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
+
+	"github.com/fvnis/golang-crud/src/controller/routes"
 )
 
 func main() {
-  err := godotenv.Load()
-  if err != nil {
-    log.Fatal("Error loading .env file")
-  }
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
 
-  router := gin.Default()
+	router := gin.Default()
 
-  routes.InitiRoutes(&router.RouterGroup)
+	routes.InitiRoutes(&router.RouterGroup)
 
-  if err := router.Run(":8080"); err != nil {
-    log.Fatal(err)
-  }
+	if err := router.Run(":8080"); err != nil {
+		log.Fatal(err)
+	}
 }
---
+++ src/controller/model/request/user_request_model.go
@@ -0,0 +1,8 @@
+package request
+
+type UserRequestModel struct {
+	Email    string
+	Password string
+	Name     string
+	Age      int8
+}
